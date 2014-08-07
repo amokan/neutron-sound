14a8b12356a5f5a2b64d04eea0f43f2fe6b4095f
jakplugg <code@neutron-sound.com>
2014-08-07 14:34:01 -0400

    added speedtest for teensyduino 1.20 rc2
    just for testing

---
+++ orgone_accumulator/official_firmware/orgone_accumulator_teensyduino120test/UPDATECONTROLS_CZALT.ino
@@ -0,0 +1,231 @@
+void UPDATECONTROLS_CZALT(){
+
+  switch (ARC+1) {
+
+  case 7:
+         
+    break;
+
+  case 9:    
+    
+     
+    break;
+
+  case 3:
+    buh = digitalReadFast(tuneLockSwitch);
+    if (tuneLockOn != buh){
+      tuneLockOn = buh;
+      if (tuneLockOn == 1) {digitalWriteFast(LED_TuneLock,1);}
+      else {digitalWriteFast(LED_TuneLock,0);}      
+    }          
+    detuneAmountCont = analogControls[2];
+    //detuneAmountCont = (detuneAmountContCubing*detuneAmountContCubing*detuneAmountContCubing)/1024.0; 
+    break;
+
+  case 4:
+
+    switch (analogControls[5]>>9){//select middle wavetable
+    case 0: 
+      waveTableMidLink = sinTable; 
+      break;
+    case 1: 
+      waveTableMidLink = triTable; 
+      break;
+    case 2: 
+      waveTableMidLink = sawTable; 
+      break;
+    case 3: 
+      waveTableMidLink = scarabTable1; 
+      break;
+    case 4: 
+      waveTableMidLink = scarabTable2; 
+      break;
+    case 5: 
+      waveTableMidLink = pulseTable; 
+      break;
+    case 6: 
+      waveTableMidLink = pnoTable; 
+      break;
+    case 7: 
+      waveTableMidLink = bassTable1; 
+      break;
+    case 8: 
+      waveTableMidLink = bassTable2; 
+      break;
+    case 9: 
+      waveTableMidLink = celloTable; 
+      break;
+    case 10: 
+      waveTableMidLink = violTable; 
+      break;
+    case 11: 
+      waveTableMidLink = distoTable; 
+      break;
+    case 12: 
+      waveTableMidLink = blipTable; 
+      break;
+    case 13: 
+      waveTableMidLink = voiceTable; 
+      break;
+    case 14: 
+      waveTableMidLink = noiseTable2; 
+      break;
+    case 15: 
+      waveTableMidLink = noiseTable; 
+      break;    
+    }      
+    break;
+
+  case 5:
+    //select wave lo wave
+    switch (analogControls[8]>>9){
+    case 0: 
+      waveTable2Link = sinTable; 
+      break;
+    case 1: 
+      waveTable2Link = triTable; 
+      break;
+    case 2: 
+      waveTable2Link = sawTable; 
+      break;
+    case 3: 
+      waveTable2Link = scarabTable1; 
+      break;
+    case 4: 
+      waveTable2Link = scarabTable2; 
+      break;
+    case 5: 
+      waveTable2Link = pulseTable; 
+      break;
+    case 6: 
+      waveTable2Link = pnoTable; 
+      break;
+    case 7: 
+      waveTable2Link = bassTable1; 
+      break;
+    case 8: 
+      waveTable2Link = bassTable2; 
+      break;
+    case 9: 
+      waveTable2Link = celloTable; 
+      break;
+    case 10: 
+      waveTable2Link = violTable; 
+      break;
+    case 11: 
+      waveTable2Link = distoTable; 
+      break;
+    case 12: 
+      waveTable2Link = blipTable; 
+      break;
+    case 13: 
+      waveTable2Link = noiseTable2; 
+      break;
+    case 14: 
+      waveTable2Link = noiseTable; 
+      break;
+    case 15: 
+      waveTable2Link = nothingTable; 
+      break;
+    
+    }      
+    break;
+
+  case 6: //select hi wave
+    
+   FMX_HiOffsetCont = (int32_t)((analogControls[4])-4096) ;
+    //FMX_HiOffsetCont = FMX_HiOffsetContCub*FMX_HiOffsetContCub*FMX_HiOffsetContCub  ;      
+
+  case 1:     
+    mixPos = (analogControls[6]>>5)<<4;     
+    
+    FMFixedOn = digitalReadFast(FMFixedSwitch);
+    if (FMFixedOn){inputConverterF = 20000;} //sets lowest fixed modulator frequency       
+    
+    oscMode = (!digitalReadFast(xModeSwitch)<<1)+ digitalReadFast(CZmodeSwitch);    
+    break; 
+
+  case 8:
+  primeDetuneOn = !digitalReadFast(primeSwitch);
+    detuneLoOn = digitalReadFast(detuneLoSwitch); 
+    detuneMidOn = !digitalReadFast(detuneMidSwitch); 
+    detuneHiOn = !digitalReadFast(detuneHiSwitch);    
+
+    break;
+
+  case 2:
+    totalratio = totalratio - readingsratio[controlAveragingIndex]; 
+    readingsratio[controlAveragingIndex] = analogControls[0];
+    totalratio = totalratio + readingsratio[controlAveragingIndex];
+    controlAveragingIndex = controlAveragingIndex + 1;
+    if (controlAveragingIndex >= numreadingsratio) controlAveragingIndex = 0;
+    averageratio = totalratio / numreadingsratio;    
+     break; 
+
+  case 10:
+    FMIndexCont = (int)(analogControls[1]>>2);
+   
+    
+//    else {
+//      FMIndexContCubing = (analogControls[8])/1024.0;
+//      FMIndexCont = (int) (FMIndexContCubing* FMIndexContCubing*FMIndexContCubing); 
+//    }
+    
+     switch (analogControls[3]>>9){
+    case 0: 
+      FMTable = sinTable; 
+      break;
+    case 1: 
+      FMTable = triTable; 
+      break;
+    case 2: 
+      FMTable = FMTableS180; 
+      break;
+    case 3: 
+      FMTable = FMTableSQ; 
+      break;
+    case 4: 
+      FMTable = FMTableSQR; 
+      break;
+    case 5: 
+      FMTable = AKWF_0003; 
+      break;
+    case 6: 
+      FMTable = pnoTable; 
+      break;
+    case 7: 
+      FMTable = bassTable1; 
+      break;
+    case 8: 
+      FMTable = bassTable2; 
+      break;
+    case 9: 
+      FMTable = celloTable; 
+      break;
+    case 10: 
+      FMTable = violTable; 
+      break;
+    case 11: 
+      FMTable = FMTableFM98; 
+      break;
+    case 12: 
+      FMTable = FMTablehvoice26; 
+      break;
+    case 13: 
+      FMTable = AKWF_squ_0011; 
+      break;
+    case 14: 
+      FMTable = noiseTable2;//FMTableCZ;
+      break;
+    case 15: 
+      FMTable = noiseTable; 
+      break;    
+    }
+    
+
+    break;   
+
+
+  }
+}
+
